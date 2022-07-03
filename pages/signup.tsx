0347098e180f8f4b46a33d9424eefea1c9566cba
유아영 <ahyoung.db@gmail.com>
2022-07-03 18:31:36 +0900

    회원가입 폼 작업중 및 useinput hook 작업

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Link from 'next/link';
 import theme from '../component/common/theme';
 import styled from 'styled-components';
@@ -6,15 +6,30 @@ import styled from 'styled-components';
 import styleLogin from '../styles/Login.module.scss';
 import HeadInfo from '../component/common/HeadInfo';
 import Loading from '../component/common/Loading';
+import useInput from '../hooks/useInput';
 
 export default function Signup() {
   const [isLoad, setIsLoad] = useState(false);
+  const [id, onChangeId] = useInput('');
+  const [password, onChangePassword] = useInput('');
+  const [passwordCheck, setPasswordCheck] = useState('');
+  const [passwordError, setPasswordError] = useState(false);
+  const [email, onChangeEmail] = useInput('');
+
+  const onChangePasswordCheck = useCallback(
+    (e) => {
+      setPasswordCheck(e.target.value);
+      setPasswordError(e.target.value !== password);
+    },
+    [password]
+  );
 
   useEffect(() => {
-    // 로드 조건 달성 시 setIsLoad(true) 처리. (ex. API 통신 및 로드 완료 시)
     setTimeout(() => {
       setIsLoad(true);
     }, 1000);
+
+    console.log(email);
   });
   return (
     <>
@@ -27,7 +42,39 @@ export default function Signup() {
 
           <SignupForm>
             <div>
-              <label htmlFor="user-email"></label>
+              <label htmlFor="user-id">아이디</label>
+              <input name="user-id" value={id} type="email" onChange={onChangeId} required />
+            </div>
+            <div>
+              <label htmlFor="user-password">비밀번호</label>
+              <input
+                name="user-password"
+                type="password"
+                value={password}
+                onChange={onChangePassword}
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="user-password-check">비밀번호체크</label>
+              <input
+                name="user-password-check"
+                value={passwordCheck}
+                onChange={onChangePasswordCheck}
+                type="password"
+                required
+              />
+              {passwordError && <ErrorMessage>비밀번호가 일치하지 않습니다.</ErrorMessage>}
+            </div>
+            <div>
+              <label htmlFor="user-email">이메일</label>
+              <input
+                name="user-email"
+                value={email}
+                type="email"
+                onChange={onChangeEmail}
+                required
+              />
             </div>
             <button
               type="button"
@@ -47,8 +94,34 @@ export default function Signup() {
 
 const SignupItem = styled.div`
   width: 32rem;
-  height: 50rem;
+  height: auto;
   margin: auto;
 `;
 
-const SignupForm = styled.form``;
+const SignupForm = styled.form`
+  div {
+    & + div {
+      margin-top: 3rem;
+    }
+
+    & + button {
+      margin-top: 4rem;
+    }
+  }
+
+  label {
+    display: block;
+    font-size: 1.4rem;
+  }
+
+  input {
+    display: block;
+    width: 100%;
+    color: #666;
+  }
+`;
+
+const ErrorMessage = styled.div`
+  font-size: 1.2rem;
+  color: red;
+`;
